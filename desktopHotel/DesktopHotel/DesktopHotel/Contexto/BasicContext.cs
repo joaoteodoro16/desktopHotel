7dfb6a01a106cda197f313558f66fc3b956fb957
joaoteodoro16 <joao.teodoro1610@gmail.com>
2021-10-03 15:53:03 -0300

    Atualizando PC

---
+++ desktopHotel/DesktopHotel/DesktopHotel/Contexto/BasicContext.cs
@@ -0,0 +1,21 @@
+﻿using DesktopHotel.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopHotel.Context
+{
+     class BasicContext: DbContext
+    {
+        //Passa a conexão
+        public BasicContext():base("Data Source=SQL5108.site4now.net;Initial Catalog=db_a79e17_bdhotel;User ID=db_a79e17_bdhotel_admin;Password=admin12345678;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        {
+        }
+
+        public DbSet<HospedeModel> Hospedes { get; set; }
+
+    }
+}
